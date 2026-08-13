306455353848072082cddde4957f2cfabb599430
rachid moutawakel <rachid.moutawakel.dev@gmail.com>
2026-08-13 15:24:22 +0200

    Add list index show and create flow

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "lists", to: "lists#index"
+  get "lists/new", to: "lists#new"
+  get "lists/:id", to: "lists#show"
+  post "lists", to: "lists#create"
+
 end
