d3734927194742439b6f31ef445717158a301c60
rachid moutawakel <rachid.moutawakel.dev@gmail.com>
2026-08-13 19:09:56 +0200

    Refactor styles and improve watch list theme

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,7 +14,7 @@ class ListsController < ApplicationController
   end
 
   def create
-  @list = List.new(list_params)
+    @list = List.new(list_params)
 
     if @list.save
       redirect_to list_path(@list)
@@ -23,10 +23,9 @@ class ListsController < ApplicationController
     end
   end
 
-
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :image_url)
   end
 end
