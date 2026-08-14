77b0a0e5fe10b6ab029e7bfe94abe1c5d3a62688
rachid moutawakel <rachid.moutawakel.dev@gmail.com>
2026-08-14 16:25:29 +0200

    Add image upload to lists

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,4 +4,7 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
   has_many :reviews, dependent: :destroy
+
+  has_one_attached :photo
 end
+ 
